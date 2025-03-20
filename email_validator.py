744d00b3f986c8df4d140d6ae90b88c2137bba54
Harish Saud <saudharish770@gmail.com>
2025-03-20 21:39:44 +0545

    Update email_validator.py

--- email_validator.py
+++ email_validator.py
@@ -1,6 +1,6 @@
 import re
 
-email = input("Enter an email address: ")
+email = input("Enter a email address: ")
 
 # Regular expression to match a valid email
 email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
@@ -8,4 +8,4 @@ email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 if re.match(email_pattern, email):
     print("Valid Email ✅")
 else:
-    print("Invalid Email ❌ Please enter valid email")
+    print("Invalid Email ❌ Please enter a valid email")
